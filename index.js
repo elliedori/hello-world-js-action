9c6ee40fa155753cbcdc6642f263d522d64a9c55
Ellie Bahadori <elliedori@gmail.com>
2020-09-03 15:09:01 -0700

    Add initiate logic

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const fs = require('fs');
 
 try {
   const jobName = core.getInput('job-name');
-  fs.writeFileSync(`job-${jobName}.txt`, `:no_entry_sign: *${jobName}* failed \n`);
+  if (jobName === "initiate-error-tracking") {
+    fs.writeFileSync(`job-${jobName}.txt`, "");
+  } else {
+    fs.writeFileSync(`job-${jobName}.txt`, `:no_entry_sign: *${jobName}* failed \n`);
+  }
 } catch (err) {
   core.setFailed(err.message);
 }
